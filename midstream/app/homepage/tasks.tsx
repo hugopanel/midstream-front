4f9ff0e9b58774ccef2bb241f96beb9a4cd15489
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-05-31 23:46:20 +0200

    HomePage automatisation

---
+++ midstream/app/homepage/tasks.tsx
@@ -0,0 +1,163 @@
+export default function Tasks() {
+    //-------Structure-----------------------------------------------------------------------------
+  
+  interface Task {
+    id: number;
+    title: string;
+    description: string;
+    type: taskType;
+    beginning_date: string;
+    end_date: string;
+    tags: Tag[];
+    priority: number;
+    status: taskStatus;
+  }
+  interface Tag {
+    id: number;
+    name: string;
+    color: tagColor;
+  }
+
+  //-------Enums a prédéfinir, voir avec tt le monde---------------------------------------------------------------------------------
+  
+  enum tagColor {
+    pink = "pink",
+    indigo = "indigo",
+    green = "green",
+  }
+  enum taskType {
+    Task = "Task",
+    Feature = "Feature",
+    Bug = "Bug",
+    Improvement = "Improvement",
+  }
+  enum taskStatus {
+    InProgress = "In Progress",
+    Ready = "Ready",
+    InReview = "In Review",
+    Done = "Done",
+  }
+
+  //---------------Html, et css à voir avec Lou cor doit étre définit-----------------------------------------------------------------------------
+  
+  // Liste des couleures prédéfinies pour les tags
+  const colorClasses: Record<tagColor, string> = {
+    pink: "bg-pink-50 text-pink-600 ring-pink-500/10",
+    indigo: "bg-indigo-50 text-indigo-600 ring-indigo-500/10",
+    green: "bg-green-50 text-green-600 ring-green-500/10",
+  };
+
+  // Liste les logos prédéfinies pour les types de tâches
+  const taskTypeIcon: Record<taskType, JSX.Element> = {
+
+    Task: ( // Logo bleu avec appareil photo
+      <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-7 grid h-16 w-16 place-items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
+                <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path>
+                <path fillRule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z" clipRule="evenodd"></path>
+                <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z"></path>
+              </svg>
+            </div>
+    ),
+    
+    Improvement: ( // Logo violet en forme d'escalier
+    <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-indigo-600 to-indigo-400 text-white shadow-indigo-500/40 shadow-lg absolute -mt-7 grid h-16 w-16 place-items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
+        <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
+      </svg>
+    </div>
+    ),
+    Feature: (  // Logo vert avec un bonhomme et un +
+    <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-green-600 to-green-400 text-white shadow-green-500/40 shadow-lg absolute -mt-7 grid h-16 w-16 place-items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
+        <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+      </svg>
+    </div>
+    ),
+    Bug: (  // Logo rose avec un bonhomme
+      <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-pink-600 to-pink-400 text-white shadow-pink-500/40 shadow-lg absolute -mt-7 grid h-16 w-16 place-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
+          <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd"></path>
+        </svg>
+      </div>
+    ),
+  };
+
+  //-----------Data (pas besoin de s'en occupé car sera pull depuis la database)---------------------------------------------------------------------------------
+  
+  // Liste des tags et leurs couleurs associées 
+  const tags: Tag[] = [
+    {id:0, name: "Front", color: tagColor.pink },
+    {id:1, name: "Back", color: tagColor.indigo },
+    {id:2, name: "Data", color: tagColor.green },
+  ];
+  
+  // Liste des tâches
+  const tasks : Task[] = [
+    {
+      id: 1,
+      title: "Link Login",
+      description: "Link the login page to the homepage",
+      type: taskType["Task"],
+      beginning_date: "2021-10-01",
+      end_date: "2021-10-02",
+      priority: 1,
+      status: taskStatus["Ready"],
+      tags: [tags[0],tags[1]]
+    },
+    {
+      id: 2,
+      title: "Plan front meeting",
+      description: "Plan a meeting to discuss the front end",
+      type: taskType["Improvement"],
+      beginning_date: "2021-10-01",
+      end_date: "2021-10-02",
+      priority: 2,
+      status: taskStatus["InProgress"],
+      tags: [tags[0],tags[1],tags[2]]
+    },
+    {
+      id: 3,
+      title: "Create Database",
+      description: "Create the database for the project",
+      type: taskType["Feature"],
+      beginning_date: "2021-10-01",
+      end_date: "2021-10-02",
+      priority: 3,
+      status: taskStatus["Ready"],
+      tags: [tags[2],tags[1]]
+    },
+    {
+      id: 4,
+      title: "Fix Menu",
+      description: "Fix the menu on the homepage",
+      type: taskType["Bug"],
+      beginning_date: "2021-10-01",
+      end_date: "2021-10-02",
+      priority: 4,
+      status: taskStatus["InProgress"],
+      tags: [tags[0]]
+    },
+  ];
+  return (
+    tasks.map((task) => (
+        <div key={task.id} className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+           
+          {/* task icon type */}
+          {taskTypeIcon[task.type]}
+          {/* task details */}
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">{task.status}</p>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">{task.title}</h4>
+          </div>
+
+          {/* tags */}
+          <div className="border-t border-blue-gray-50 p-4">
+            {task.tags.map((tag) => (
+                <span key={tag.name} className={"inline-flex items-center rounded-md px-4 py-2 text-sm font-medium ring-1 ring-inset mr-[10px] " + colorClasses[tag.color]}> {tag.name} </span>
+              ))}
+          </div>
+        </div>
+      ))
+    );
+}
