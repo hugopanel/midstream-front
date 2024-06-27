fd9dd6d9ed2b03b9445fdf928c2302535a2f49c4
paulinedavid <pauline.david@efrei.net>
2024-06-27 19:40:01 +0200

    task delete dans yask_edit

--- midstream/app/tasks_edit/page.tsx
+++ midstream/app/tasks_edit/page.tsx
@@ -193,6 +193,32 @@ export default function NewTask() {
         }
     }, []);
 
+    const deleteTask = async () => {    
+
+        console.log("delete task");
+        console.log(taskId);
+        
+        try {
+            const response = await fetch('/api/delete_task', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ taskId }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || 'Failed to get the members of the team');
+            }
+
+            const data = await response.json();
+            router.push('/tasks');
+        } catch (error: any) {
+            console.error('Error logging in:', error);
+        }
+    };
+
 
     
     return (
@@ -246,7 +272,7 @@ export default function NewTask() {
                                 <label htmlFor="description" className="text-gray-700">
                                     Description
                                 </label>
-                                <textarea name="description" id="description"value={description} onChange={handleDescriptionChange}  placeholder="Describe the task..." className="w-full mb-8 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                <textarea name="description" id="description" maxLength={50} value={description} onChange={handleDescriptionChange}  placeholder="Describe the task..." className="w-full mb-8 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                                 ></textarea>
 
                             </div>
@@ -301,7 +327,7 @@ export default function NewTask() {
                                     Save
                                 </span>
                             </button>
-                            <button onClick={() => { window.location.href = '/tasks' }}
+                            <button onClick={deleteTask}
                                 className="mt-10 tracking-wide font-semibold bg-gradient-to-tl from-orange-500 to-red-500 text-gray-100 w-full py-4 rounded-lg hover:bg-gradient-to-tr from-blue-500 to-indigo-500 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
