743c07201a64e46512fcea577587e1955112b01a
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-07-01 17:17:08 +0200

    delete document

--- midstream/app/doc_explorer/page.tsx
+++ midstream/app/doc_explorer/page.tsx
@@ -49,6 +49,25 @@ export default function DocExplorer() {
         setFilteredDocuments(Array.isArray(documents) ? documents.filter((doc) => doc.name.toLowerCase().includes(submitSearch.toLowerCase())) : []);
     }, [documents, submitSearch]);
 
+    const handleDelete = (id: string) => async () => {
+        const response = await fetch('/api/document_delete', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ documentId: id }),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.log(errorData);
+            throw new Error(errorData.message || 'Failed to delete document');
+        }
+
+        const data = await response.json();
+        console.log(data);
+        fetchDocuments();
+    }
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value);
     const handleSearchSubmit = (e: React.FormEvent) => { e.preventDefault(); setSubmitSearch(search); };
     return (
@@ -123,7 +142,7 @@ export default function DocExplorer() {
                                                         </div>
                                                     </td>
                                                     <td className="py-3 px-5 border-b border-blue-gray-50">
-                                                        <button aria-expanded="false" aria-haspopup="menu" id=":r2:" className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-700 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30" type="button">
+                                                        <button onClick={ handleDelete(doc.id) } aria-expanded="false" aria-haspopup="menu" id=":r2:" className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-700 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30" type="button">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                             </svg>
